0bdddc32c284eee601739b58eda45827ea8c57d5
chubu <moomost123@gmail.com>
2024-01-06 19:30:42 +0700

    fix bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,8 +79,8 @@ export default function Navbar({ children }) {
                     </button>
                   ) : (
                     <Link href="#" onClick={() => {
-                      menuItem.text === "signOut" ? signOut({ callbackUrl: '/' }) : "";
-                    }} className={`${menuItem.text === "signOut" ? session ? "block" : "hidden" : ""} block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black`}>
+                      menuItem.text === "ออกจากระบบ" ? signOut({ callbackUrl: '/' }) : "";
+                    }} className={`${menuItem.text === "ออกจากระบบ" ? session ? "block" : "hidden" : ""} block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black`}>
                       {menuItem.text}
                     </Link>
                   )}
